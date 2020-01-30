d842c4c2c6d32d663b5567da0724db8af42f2d6b
Marcos <markettes_99@outlook.es>
2020-01-30 20:11:39 +0100

    Canvas

--- app/src/main/java/devcom/marcos/triangularpos/MainActivity.java
+++ app/src/main/java/devcom/marcos/triangularpos/MainActivity.java
@@ -2,13 +2,37 @@ package devcom.marcos.triangularpos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        View vista = new MyView(this);
+        setContentView(vista);
+    }
+
+    class MyView extends View {
+        public MyView(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            Paint p = new Paint();
+            p.setStyle(Paint.Style.STROKE);
+            p.setStrokeWidth(5);
+            p.setColor(Color.BLUE);
+
+            canvas.drawRect(50, 200, 40, 40, p);
+            canvas.drawLine(30,300,80,1500,p);
+
+        }
     }
 }
