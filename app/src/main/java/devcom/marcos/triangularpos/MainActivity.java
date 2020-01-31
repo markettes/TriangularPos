3a44b0a4cde5c82820f9a4299f42179199cc9a4d
Marcos <33052423+markettes@users.noreply.github.com>
2020-01-31 13:00:47 +0100

    trilateration immplemented
    trilateration library by lemmingapex

--- app/src/main/java/devcom/marcos/triangularpos/MainActivity.java
+++ app/src/main/java/devcom/marcos/triangularpos/MainActivity.java
@@ -1,5 +1,6 @@
 package devcom.marcos.triangularpos;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import devcom.marcos.triangularpos.trilateration.Point;
 import devcom.marcos.triangularpos.trilateration.Trilateration;
@@ -11,6 +12,14 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 
+import com.lemmingapex.trilateration.NonLinearLeastSquaresSolver;
+import com.lemmingapex.trilateration.TrilaterationFunction;
+
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresOptimizer;
+import org.apache.commons.math3.fitting.leastsquares.LevenbergMarquardtOptimizer;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+
 import static devcom.marcos.triangularpos.trilateration.Trilateration.Compute;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         View vista = new MyView(this);
         setContentView(vista);
+
+
     }
 
     class MyView extends View {
@@ -34,18 +45,31 @@ public class MainActivity extends AppCompatActivity {
             p.setStrokeWidth(60);
             p.setColor(Color.BLUE);
 
-            canvas.drawCircle(400, 300, 30, p);
-            canvas.drawCircle(500, 650, 30, p);
-            canvas.drawCircle(651, 350, 30, p);
+            canvas.drawCircle(100, 100, 30, p);
+            canvas.drawCircle(200, 200, 30, p);
+            canvas.drawCircle(300, 100, 30, p);
 
-            Point p1 = new Point(400,300,150);
-            Point p2 = new Point(500,650,250);
-            Point p3 = new Point(651,350,235);
+            Point p1 = new Point(100,100,100);
+            Point p2 = new Point(200,200,100);
+            Point p3 = new Point(300,100,100);
 
-            double[] pf = Trilateration.Compute(p1,p2,p3);
+            double[][] positions = new double[][] { { 100.0, 100.0 }, { 200.0, 200.0 },
+                    { 300.0, 100.0 } };
+            double[] distances = new double[] { 100.0, 100.0, 100.0 };
 
-            canvas.drawCircle((float)pf[0], (float)pf[1], 30, p);
+            NonLinearLeastSquaresSolver solver = new NonLinearLeastSquaresSolver(
+                    new TrilaterationFunction(positions, distances),
+                    new LevenbergMarquardtOptimizer());
+            LeastSquaresOptimizer.Optimum optimum = solver.solve();
 
+            // the answer
+            double[] centroid = optimum.getPoint().toArray();
+
+            // error and geometry information; may throw SingularMatrixException depending the threshold argument provided
+            RealVector standardDeviation = optimum.getSigma(0);
+            RealMatrix covarianceMatrix = optimum.getCovariances(0);
+
+            canvas.drawCircle((float)centroid[0], (float)centroid[1], 30, p);
         }
     }
 }
